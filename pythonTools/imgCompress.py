a6e1b71b4258b339c66a46aa782bd1d99e7cffbc
Matthew Morrison <mcharlesmorrison@berkeley.edu>
2023-04-28 08:16:32 -0700

    tbolt

--- pythonTools/imgCompress.py
+++ pythonTools/imgCompress.py
@@ -1,6 +1,6 @@
 import os
 from PIL import Image
-def compress_images(directory=None, quality=30):
+def compress_images(directory=None, quality=60):
     if directory is None:
         directory = os.getcwd()
     for filename in os.listdir(directory):
@@ -17,4 +17,4 @@ def compress_images(directory=None, quality=30):
                 #     return False
     return True # Return True if no images were found in the directory
 
-print(compress_images("/Users/mattmorrison/Desktop/morrison"))
+print(compress_images("/Users/mattmorrison/Desktop/extra"))
