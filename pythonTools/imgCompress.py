9ad1ad0b9b3d836536fa24f5e05009a688e30d98
Matt Morrison <mcharlesmorrison@berkeley.edu>
2024-08-19 12:22:11 -0700

    clouds rest tr

--- pythonTools/imgCompress.py
+++ pythonTools/imgCompress.py
@@ -24,7 +24,7 @@ def compress_images(directory=None, quality=70):
 
 
 def main():
-    print(compress_images("cassLogger"))
+    print(compress_images("cloudsrest"))
 
 
 if __name__ == "__main__":
