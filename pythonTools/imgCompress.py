ecd3d123ee83c2f7655a579eeb3153b0fa1f4a32
Matt Morrison <mcharlesmorrison@berkeley.edu>
2025-03-26 10:20:22 -0700

    Saline TR

--- pythonTools/imgCompress.py
+++ pythonTools/imgCompress.py
@@ -6,6 +6,14 @@ def compress_images(directory=None, quality=70):
     directory = "/Users/mattmorrison/Desktop/" + directory
     if directory is None:
         directory = os.getcwd()
+
+    # Calculate total size before compression
+    total_size_before = sum(
+        os.path.getsize(os.path.join(directory, f))
+        for f in os.listdir(directory)
+        if f.casefold().endswith(".jpg") or f.casefold().endswith(".jpeg")
+    )
+
     for filename in os.listdir(directory):
         if filename.casefold().endswith(".jpg") or filename.casefold().endswith(
             ".jpeg"
@@ -13,18 +21,26 @@ def compress_images(directory=None, quality=70):
             filepath = os.path.join(directory, filename)
             with Image.open(filepath) as im:
                 im.save(filepath, optimize=True, quality=quality)
-                # # Check if the file size decreased after compression
-                # if os.path.getsize(filepath) < os.path.getsize(filepath[:-4] + '_original.jpg'):
-                #     os.remove(filepath[:-4] + '_original.jpg')
-                #     return True
-                # else:
-                #     os.remove(filepath)
-                #     return False
+
+    # Calculate total size after compression
+    total_size_after = sum(
+        os.path.getsize(os.path.join(directory, f))
+        for f in os.listdir(directory)
+        if f.casefold().endswith(".jpg") or f.casefold().endswith(".jpeg")
+    )
+
+    # Convert sizes to megabytes
+    total_size_before_mb = total_size_before / (1024 * 1024)
+    total_size_after_mb = total_size_after / (1024 * 1024)
+
+    print(f"Total size before compression: {total_size_before_mb:.2f} MB")
+    print(f"Total size after compression: {total_size_after_mb:.2f} MB")
+
     return True  # Return True if no images were found in the directory
 
 
 def main():
-    print(compress_images("northern6"))
+    print(compress_images("saline"))
 
 
 if __name__ == "__main__":
